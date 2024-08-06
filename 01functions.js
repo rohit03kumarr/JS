c0e468d23a9c53c93c0ff9506496e00950a0d61f
Rohit Kumar <rohot07kumar@gmail.com>
2024-08-06 07:03:55 +0000

    functions done

---
+++ 01functions.js
@@ -0,0 +1,92 @@
+//+++++++++++++++++++++++++   function1 +++++++++++++++++++
+
+/* function username(fname,lname){
+    console.log(fname + lname);
+ }
+username("rohit","kumar")
+
+output : rohitkumar
+*/
+
+//++++++++++++++++++++++++++   function2   +++++++++++++++++++++++++++++++++++++++++++++
+
+
+/*function data(){
+    name="rohit kumar",
+    age =20,
+    email ="rohit@gmail.com",
+    contact= 7717526532
+console.log(name,age,email,contact)
+}
+data("name","age","email","contact")
+
+//`${this.name} ${this.age} ${this.email} ${contact}`
+
+output : rohitkumar 20 rohit@gmail.com  7717526532 */
+
+
+//++++++++++++++++++++++++   function3    +++++++++++++++++++++++
+
+/*function input(){
+  name = "rohit",
+  email = "abc@gmail.com",
+  contact = "7717526532"
+  console.log(name,email,contact)
+}
+input()
+
+output: rohit abc@gmail.com  7717526532*/
+
+//++++++++++++++++++++++++ function4  +++++++++++++++++
+
+/*function calculatecartPrice(val1,val2,...num1){
+val1 = 100,
+val2 = 200
+return num1
+}
+console.log(calculatecartPrice(300,400,500))
+
+output: [500]*/
+
+
+//++++++++++++++++++++++++ function5  +++++++++++++++++++++++++++++++
+
+/*
+function calculatecartPrice(...num1){
+    return num1
+}
+console.log(100,200,300,400,500)
+
+output: 100,200,300,400,500*/
+
+
+//++++++++++++++++++++++  function6  with objects  +++++++++++++++++
+
+/*
+++++++++++++++++++++++++++++  creating object  +++++++++++++++++++++
+
+const user = {
+  username :"rohitkumar",
+  age: 20
+}
+++++++++++++++++++++++++++++++++ make function of object +++++++++++++++++++++++++++
+
+function handleObject(user){
+  console.log(`username is ${user.username} and age is ${user.age}`)
+}
+
+handleObject(user)
+
+output: username is rohitkumar ans age is 20  */
+
+//++++++++++++++++++++++++++++++++  function7  with array  ++++++++++++++++++++++++
+
+/*  
+const newArray = [100,200,300]     (creating a array)
+
+function secondValue(getArray){     (function declaring)     
+   return getArray[2]
+}
+console.log(secondValue(newArray));    (printing array)
+
+*/
