5d6705bfddf8829b1ecb211e62058b3899b8f509
Rohit Kumar <rohot07kumar@gmail.com>
2024-08-07 06:58:47 +0000

    arrow and this done

---
+++ 03arrow.js
@@ -0,0 +1,32 @@
+const user ={
+    username: "rohitkumar",
+    price: 999,
+    welcomeMessage: function(){
+        console.log(`${this.username},Welcome to Website`);
+    }
+
+}
+//user.welcomeMessage()
+//user.username = "deepakkumar"
+//user.welcomeMessage()
+
+// function chai(){
+//     let username = "rohitkumar"
+//  console.log(this.username);
+// }
+// chai()
+
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// console.log(addTwo(3,4))
+
+// const addTwo = (num1, num2) =>  num1 + num2 //implecity return
+
+/*const object = (num1, num2) => ({username:"rohitkumar"})
+console.log(object(3,4))
+output: username: 'rohitkuamr'*/
+
+
+
