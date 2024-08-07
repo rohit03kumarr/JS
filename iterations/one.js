c016ad9d79631187a23516e8a12e1f4a991223b9
Rohit Kumar <rohot07kumar@gmail.com>
2024-08-07 11:48:30 +0000

    foor loop done

---
+++ iterations/one.js
@@ -0,0 +1,20 @@
+
+//  ++++++++++++++++++  for loop  ++++++++++++++
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//       //  console.log("5 is best number");
+        
+//     }
+//     //console.log(element); 
+// }
+
+for (let i = 0; i <= 10; i++) {
+   // console.log(`outer loop values: ${i}`);
+    for (let j = 0; j <=10; j++) {
+     //console.log(`Inner loop value ${j} `);   
+   //console.log(i + '*' + j + ' = ' + i*j );
+   
+    }  
+}
