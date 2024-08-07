56171668be0cd1dd0630b392e0f8397c84ed16e0
Rohit Kumar <rohot07kumar@gmail.com>
2024-08-07 08:03:14 +0000

    control flow

---
+++ controlflow/one.js
@@ -0,0 +1,19 @@
+// if statement
+
+/*const isLoggedIN = true
+
+if (true) {
+  console.log(`User is Logged In`)
+}
+output: User isLogged In
+//  <, >, <=, >=, ==, !=, ===, !==*/
+
+// ++++++++++++++++++++++++ example2 if else statement++++++++++++++++++++
+
+const temperature = 51
+if (temperature < 50 ) {
+    console.log(`Temperature is lower than 50`);
+} else
+{
+console.log(`Temperature is higher than 50 `);
+}
