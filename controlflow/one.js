d59111e6431e9db17b21fd2abaf1c03df0591530
Rohit Kumar <rohot07kumar@gmail.com>
2024-08-07 10:17:38 +0000

    if statement and if else statement done.

--- controlflow/one.js
+++ controlflow/one.js
@@ -10,10 +10,67 @@ output: User isLogged In
 
 // ++++++++++++++++++++++++ example2 if else statement++++++++++++++++++++
 
-const temperature = 51
-if (temperature < 50 ) {
-    console.log(`Temperature is lower than 50`);
-} else
-{
-console.log(`Temperature is higher than 50 `);
-}
+// const temperature = 51
+// if (temperature < 50 ) {
+//     console.log(`Temperature is lower than 50`);
+// } else
+// {
+// console.log(`Temperature is higher than 50 `);
+// }
+//output: Tempreature is higher than 50 
+
+//     +++++++++++++++++++  example3  ++++++++++++++++
+// const score = 200
+// if (score > 100 ) {
+//   const power =  "fly"
+//   console.log(`userpower: ${power}`);
+  
+// console.log(`score is greater than 100`);  
+// }
+
+/*output: userpower: fly 
+         score is greater than 50 */
+
+ //  ++++++++++++++++++  example4  +++++++++++++++++++++
+ 
+//  const balance = 1200
+//  if (balance < 1000){
+//   console.log("balance is less than 1000");
+//  }
+//  else{
+//   console.log(`balance is more than 1000`);
+//  }
+//  output: balance is more than 1000
+
+//  +++++++++++++++++ example5 (if else)+++++++++++++++++++
+
+// const balance = 800
+// if (balance < 500) {
+//   console.log(`less than 500`);
+// }
+// else if (balance > 750) {
+//   console.log(`balance is greater than 750 `);
+// }
+/* if balance is less than 500 output will be balance less than 500,
+if balance is greater than 750 output will be balance is greater than 750.*/
+
+//  +++++++++++++++++ example 6 (real world) +++++++++++++++
+
+// const username = `rohitkumar`
+// const userLoggedIn = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = false
+
+// if (userLoggedIn && loggedInFromGoogle || loggedInFromEmail) {
+//     console.log(`user is Logged In`); 
+// } else{
+//   console.log(`User is not Logged In`);
+// }
+
+//  && (AND)is used to check both Left,right conditions. 
+//  ||  (OR) is used to check multiple conditions.
+
+
+//  +++++++++++++++++ example7 +++++++++++++++++++++
+
+
