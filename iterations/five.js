4cea7257e3f44006f05d0abc1a2904c67e9b013f
Rohit Kumar <rohot07kumar@gmail.com>
2024-08-08 09:48:43 +0000

    for each loop done in array

---
+++ iterations/five.js
@@ -0,0 +1,41 @@
+
+// // +++++++++++++++++  for each loop +++++++++++++++=====
+
+//const coding = ['js','ruby','python','java']
+// //coding.forEach( function (item) {
+     //console.log(item);
+// })
+
+// coding.forEach( (item) => {
+    //console.log(item);    
+// })
+
+
+ const coding = ['js','ruby','python','java']
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe)
+
+coding.forEach( (item,index,arr) => {
+    //console.log(item,index,arr);
+    
+});
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+       languageName: "python",
+        languageFileName: "py"
+    }
+]
+myCoding.forEach( (item) => {
+    console.log(item.languageFileName);  
+})
