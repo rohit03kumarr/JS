3d163ee6b542bb185891c42c70affcd5a3252bc0
Rohit Kumar <rohot07kumar@gmail.com>
2024-08-08 08:21:21 +0000

    do loop and do while loop done

---
+++ iterations/two.js
@@ -0,0 +1,30 @@
+
+// ++++++++++++++++++++  while loop  ++++++++++++
+    
+// let index = 0 
+// while (index <= 10 ) {
+//     console.log(`value of index is ${index}`);
+//     index = index +2 
+// }
+
+//  let myArray = ["rohit","deepak", "kumar"]
+// let arr = 0 
+//  while (arr < myArray.length) {     
+// console.log(`value is ${myArray[arr]}`);
+// arr = arr +1
+// }
+
+// let myArray = ["rohit","deepak","kumar"]
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+//}
+
+//  ++++++++++++++++  do while  +++++++++++++++++
+
+
+// let Score = 1
+// do {
+//     console.log(`Score is  ${Score}`);
+//     Score++
+// } while ( Score <= 10);
