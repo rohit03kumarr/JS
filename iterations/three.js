42b986c6b788b082a479a8be57c600587974abe7
Rohit Kumar <rohot07kumar@gmail.com>
2024-08-08 08:44:15 +0000

    maps and for-of loops done

---
+++ iterations/three.js
@@ -0,0 +1,31 @@
+// ++++++++++++++++++  for-of    ++++++++++++++
+
+// const arr = [1,2,3,4,5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const arr = ['rohit','kumar','deepak']
+// for (const name of arr) {
+//     console.log(name); 
+// }
+
+// const greeting = "welcome"
+// for (const message  of greeting) {
+//     //console.log(`hello ${message}`);    
+// }
+
+
+// ++++++++++++++++++++ maps +++++++++++++++
+
+// const map = new Map()
+// map.set('IN',"INDIA" )
+// map.set('USA',"UNITED STATES OF AMERICA")
+// map.set('FR',"FRANCE")
+// //console.log(map);
+
+// for (const [key,value] of map) {
+//     console.log(key, ':-', value); 
+// }
+
